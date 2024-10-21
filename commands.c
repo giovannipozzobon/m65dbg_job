269bcd1e06bd4b51293f785822cc6197b471d68f
Gurce Isikyildiz <gurce.isikyildiz@gmail.com>
2024-10-21 01:03:26 +0100

    a similar fix for 'val' command's character value

--- commands.c
+++ commands.c
@@ -4487,8 +4487,12 @@ void cmdPrintValue(void)
     val = get_sym_value(strVal);
 
   char charval[64] = "";
-  if (val >= 32 && val < 256)
+  if (val >= 32 && val < 256) {
+    if (val >= 192 && val <= 223)
+      val -= 96;
+
     sprintf(charval, "(char='%c')", val);
+  }
 
   printf("  $%04X  /  %d  /  %%%s %s\n", val, val, toBinaryString(val), charval);
 }
